6f050ebb8bf2dc8c7a2e13342dc5e4d29c571156
Nalipp <nateplipp@gmail.com>
2016-08-10 15:21:10 +0900

    add delete funcitonality

--- todo.rb
+++ todo.rb
@@ -1,6 +1,9 @@
+require 'pry'
+
 require 'sinatra'
 require 'sinatra/reloader'
 require 'tilt/erubis'
+require 'sinatra/content_for'
 
 configure do
   enable :sessions
@@ -8,7 +11,7 @@ configure do
 end
 
 before do
-  session[:lists] ||= []
+  @lists = session[:lists] ||= []
 end
 
 get '/' do
@@ -16,14 +19,14 @@ get '/' do
 end
 
 # View all lists
-get '/lists' do
+get "/lists" do
   @lists = session[:lists]
   erb :lists, layout: :layout
 end
 
 # Render the new list form
-get '/lists/new' do
-  erb :new_list
+get "/lists/new" do
+  erb :new_list, layout: :layout
 end
 
 # Return an error message if the name is invalid. Otherwise we will return nil
@@ -31,7 +34,7 @@ def error_for_list_name(name)
   if !(1..100).cover? name.length
     'The list name must be between 1 and 100 characters'
   elsif session[:lists].any? { |list| list[:name] == name }
-    'That list was already created!'
+    'That list already exists!'
   end
 end
 
@@ -48,4 +51,44 @@ post '/lists' do
     session[:success] = 'The list has been created!'
     redirect '/lists'
   end
+end
+
+# Display a todo list
+get '/lists/:id' do
+  id = params[:id].to_i
+  @list = session[:lists][id]
+  erb :list, layout: :layout
+end
+
+# Edit an existing todo list
+get '/lists/:id/edit' do
+  id = params[:id].to_i
+  @list = session[:lists][id]
+  erb :edit_list, layout: :layout
+end
+
+# Update an existing todo list
+post '/lists/:id' do
+  id = params[:id].to_i
+  @list = session[:lists][id]
+
+  list_name = params[:list_name].strip
+
+  error = error_for_list_name(list_name)
+  if error
+    session[:error] = error
+    erb :edit_list, layout: :layout
+  else
+    @list[:name] = list_name
+    session[:success] = 'The list has been updated!'
+    redirect "/lists/#{id}"
+  end
+end
+
+# Update an existing todo list
+post '/lists/:id/delete' do
+  id = params[:id].to_i
+  session[:lists].delete_at(id)
+  session[:success] = 'The list has been deleted!'
+  redirect "/lists"
 end
