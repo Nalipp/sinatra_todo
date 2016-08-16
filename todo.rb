3a91b1b9b611c3549a661410452005d544a92bbf
Nalipp <nateplipp@gmail.com>
2016-08-16 18:03:08 +0900

    add ajax to delete requests

--- todo.rb
+++ todo.rb
@@ -157,8 +157,12 @@ end
 post '/lists/:id/delete' do
   id = params[:id].to_i
   session[:lists].delete_at(id)
-  session[:success] = 'The list has been deleted!'
-  redirect "/lists"
+  if env["HTTP_X_REQUESTED_WITH"] == "XMLHttpRequest"
+    "/lists"
+  else
+    session[:success] = 'The list has been deleted!'
+    redirect "/lists"
+  end
 end
 
 # Delete a todo from a list
@@ -168,8 +172,12 @@ post '/lists/:list_id/todos/:id/delete' do
 
   id = params[:id].to_i
   @list[:todos].delete_at(id)
-  session[:success] = 'The list has been deleted!'
-  redirect "/lists/#{@list_id}"
+  if env["HTTP_X_REQUESTED_WITH"] == "XMLHttpRequest"
+    status 204
+  else
+    session[:success] = 'The list has been deleted!'
+    redirect "/lists/#{@list_id}"
+  end
 end
 
 #Update the status of a todo
